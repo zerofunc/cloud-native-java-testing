a13ba1bbf7a20058a60dc4ca02c37a6c2daae72a
유형주 <yhj0429@midasit.com>
2019-03-04 20:51:01 +0900

    Init basic test

---
+++ basic-test/src/main/java/hj/testing/basictest/BasicTestApplication.java
@@ -0,0 +1,13 @@
+package hj.testing.basictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTestApplication.class, args);
+	}
+
+}
---
+++ basic-test/src/test/java/hj/testing/basictest/BasicTestApplicationTests.java
@@ -0,0 +1,16 @@
+package hj.testing.basictest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
